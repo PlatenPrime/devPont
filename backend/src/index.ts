83e561b6fb859f86141616e560e78aca63bcf015
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-01-21 08:10:52 +0200

    21.01.2025

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,8 @@
 console.log("Hello, World!");
 console.log("Hello, World!");
-console.log("Hello, World!");
+function some() {
+  console.log("Hello, World!");
+  for (let i = 0; i < 10; i++) {
+    console.log("Hello, World!");
+  }
+}
