d10e225e2702d4d59c1f9412f1c21b5392a3b696
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-01-23 06:25:08 +0200

    install express and add one test endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,25 @@
-console.log("Hello, World!");
-console.log("Hello, World!");
-function some() {
-  console.log("Hello, World!");
-  for (let i = 0; i < 10; i++) {
-    console.log("Hello, World!");
-  }
-}
+import express from "express";
+
+
+const fos = [
+  { id: 1, title: "FO 1", description: "Description of FO1" },
+  { id: 2, title: "FO 2", description: "Description of FO2" },
+  { id: 3, title: "FO 3", description: "Description of FO3" },
+  { id: 4, title: "FO 4", description: "Description of FO4" },
+  { id: 5, title: "FO 5", description: "Description of FO5" },
+];
+
+const expressApp = express();
+
+expressApp.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+expressApp.get("/fos", (req, res) => {
+  res.send(fos);
+});
+
+
+expressApp.listen(3000, () => {
+  console.info("Listening at http://localhost:3000");
+});
